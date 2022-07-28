f65f58d33582a66f9ba6e4efc89116dc90785843
chloe.bm <chloe@scenify.io>
2022-07-27 19:34:47 -0500

    improve video preview

--- src/views/DesignEditor/components/Preview/VideoPreview.tsx
+++ src/views/DesignEditor/components/Preview/VideoPreview.tsx
@@ -5,7 +5,7 @@ import ReactPlayer from "react-player"
 function VideoPreview({ src }: { src: string }) {
   return (
     <Block style={{ display: "flex" }}>
-      <ReactPlayer id="samsam" width={"100%"} height={"100%"} controls autoPlay url={src} />
+      <ReactPlayer muted={false} className="react-player" width={"100%"} height={"100%"} controls autoPlay url={src} />
     </Block>
   )
 }
