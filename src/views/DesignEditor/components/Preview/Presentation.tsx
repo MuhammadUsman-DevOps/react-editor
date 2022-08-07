fd8d96ade2d1e97ac3d6533717da04920060e974
chloe.bm <chloe@scenify.io>
2022-08-07 18:11:02 -0500

    improve preview for presentations

--- src/views/DesignEditor/components/Preview/Presentation.tsx
+++ src/views/DesignEditor/components/Preview/Presentation.tsx
@@ -1,11 +1,46 @@
 import React from "react"
 import { Block } from "baseui/block"
-import "react-responsive-carousel/lib/styles/carousel.min.css"
-import useDesignEditorPages from "~/hooks/useDesignEditorScenes"
+import useDesignEditorScenes from "~/hooks/useDesignEditorScenes"
 import { Carousel } from "react-responsive-carousel"
+import { useEditor } from "@scenify/react"
+import { IDesign } from "@scenify/types"
+import Loading from "~/components/Loading"
+import "react-responsive-carousel/lib/styles/carousel.min.css"
 
 export default function () {
-  const pages = useDesignEditorPages()
+  const [slides, setSlides] = React.useState<{ id: string; preview: string }[]>([])
+  const scenes = useDesignEditorScenes()
+  const editor = useEditor()
+  const [loading, setLoading] = React.useState(true)
+
+  const loadSlides = React.useCallback(
+    async (scenes: IDesign[]) => {
+      const slides = []
+      for (const scene of scenes) {
+        const preview = (await editor.renderer.render(scene)) as string
+        slides.push({
+          id: scene.id,
+          preview,
+        })
+      }
+      setSlides(slides)
+      setLoading(false)
+    },
+    [editor]
+  )
+
+  React.useEffect(() => {
+    if (scenes && editor) {
+      const currentScene = editor.design.exportToJSON()
+      const updatedScenes = scenes.map((scene) => {
+        if (scene.id === currentScene.id) {
+          return currentScene
+        }
+        return scene
+      })
+      loadSlides(updatedScenes)
+    }
+  }, [editor, scenes])
 
   return (
     <Block
@@ -18,11 +53,15 @@ export default function () {
       }}
     >
       <Block $style={{ position: "absolute", maxWidth: "840px" }}>
-        <Carousel showIndicators={false} showThumbs={false} useKeyboardArrows={true} showStatus={false}>
-          {pages.map((page, index) => (
-            <img width={"auto"} height={"100%"} key={index} src={page.preview} />
-          ))}
-        </Carousel>
+        {loading ? (
+          <Loading />
+        ) : (
+          <Carousel showIndicators={false} showThumbs={false} useKeyboardArrows={true} showStatus={false}>
+            {slides.map((page, index) => (
+              <img width={"auto"} height={"100%"} key={index} src={page.preview} />
+            ))}
+          </Carousel>
+        )}
       </Block>
     </Block>
   )
