1d71a851cd30412cbdd47afaa0e50cce2d0c94ea
chloe.bm <chloe@scenify.io>
2022-08-20 10:46:27 -0500

    update config for development

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import * as path from "path"
+import tsconfigPaths from "vite-tsconfig-paths"
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: "~", replacement: path.resolve(__dirname, "/src") }],
-  },
+  plugins: [react(), tsconfigPaths()],
 })
