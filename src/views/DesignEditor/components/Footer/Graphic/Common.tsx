9c5cef70cb4d01c036f71e68f886536e9498cef1
Rashmi Ranjan Bisoi <rushmedev@gmail.com>
2022-08-21 11:47:56 +0530

    Migrated zoom in/out functionality.

--- src/views/DesignEditor/components/Footer/Graphic/Common.tsx
+++ src/views/DesignEditor/components/Footer/Graphic/Common.tsx
@@ -1,20 +1,33 @@
 import React from "react"
-import { styled } from "baseui"
-import { Theme } from "baseui/theme"
 import Icons from "~/components/Icons"
 import { Button, KIND, SIZE } from "baseui/button"
-import { useZoomRatio } from "@layerhub-io/react"
+import { useZoomRatio, useEditor } from "@layerhub-io/react"
 import { Block } from "baseui/block"
+import { Slider } from "baseui/slider"
+import { Input } from "baseui/input"
 
 interface Options {
   zoomRatio: number
 }
 
 export default function () {
+  const editor = useEditor()
   const [options, setOptions] = React.useState<Options>({
     zoomRatio: 20,
   })
   const zoomRatio: number = useZoomRatio()
+
+  const handleChange = (type: string, value: any) => {
+    if (value < 0) {
+      editor.zoom.zoomToRatio(1 / 100)
+    }
+    else if (value > 240) {
+      editor.zoom.zoomToRatio(240 / 100)
+    }
+    else {
+      editor.zoom.zoomToRatio(value / 100)
+    }
+  }
 
   React.useEffect(() => {
     setOptions({ ...options, zoomRatio: Math.round(zoomRatio * 100) })
@@ -34,6 +47,61 @@ export default function () {
         <Button kind={KIND.tertiary} size={SIZE.compact}>
           <Icons.Layers size={20} />
         </Button>
+      </div>
+      <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+        <Button kind={KIND.tertiary} size={SIZE.compact}>
+          <Icons.Expand size={16} />
+        </Button>
+        <Button kind={KIND.tertiary} size={SIZE.compact}>
+          <Icons.Compress size={16} />
+        </Button>
+        <Button kind={KIND.tertiary} size={SIZE.compact}
+          onClick={() => handleChange("zoomRatio", options.zoomRatio - 20)}>
+          <Icons.RemoveCircleOutline size={24} />
+        </Button>
+        <Slider
+          overrides={{
+            InnerThumb: () => null,
+            ThumbValue: () => null,
+            TickBar: () => null,
+            Root: {
+              style: { width: "140px" },
+            },
+            Thumb: {
+              style: {
+                height: "12px",
+                width: "12px",
+                paddingLeft: 0,
+              },
+            },
+            Track: {
+              style: {
+                paddingLeft: 0,
+                paddingRight: 0,
+              },
+            },
+          }}
+          value={[options.zoomRatio]}
+          onChange={({ value }) => { handleChange("zoomRatio", value[0]) }}
+          min={10}
+          max={240}
+        />
+        <Button kind={KIND.tertiary} size={SIZE.compact}
+          onClick={() => handleChange("zoomRatio", options.zoomRatio + 20)}>
+          <Icons.AddCircleOutline size={24} />
+        </Button>
+        <Input
+          value={options.zoomRatio}
+          endEnhancer="%"
+          overrides={{
+            Root: {
+              style: {
+                width: "96px",
+              },
+            },
+          }}
+          size={SIZE.mini}
+        />
       </div>
       <div style={{ display: "flex", alignItems: "center", justifyContent: "end" }}>
         <Button kind={KIND.tertiary} size={SIZE.compact}>
