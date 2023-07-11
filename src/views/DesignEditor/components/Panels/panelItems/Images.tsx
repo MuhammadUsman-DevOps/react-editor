68cddaaa56433fcc3d7fff048cfea5eca10b6682
Prudhvi472 <prudhvi5499@gmail.com>
2023-07-11 16:07:45 +0530

    image overlay added

--- src/views/DesignEditor/components/Panels/panelItems/Images.tsx
+++ src/views/DesignEditor/components/Panels/panelItems/Images.tsx
@@ -9,6 +9,7 @@ import useSetIsSidebarOpen from "~/hooks/useSetIsSidebarOpen"
 import Input from "baseui/input/input"
 import { Button } from "baseui/button"
 import image from "~/core/parser/image"
+import './images.css'
 
 const Images = () => {
   const editor = useEditor()
@@ -30,7 +31,7 @@ const Images = () => {
   const fetching = async (e: any) => {
   
     e.preventDefault();
-console.log(input);
+    console.log(input);
     const headers = new Headers();
   headers.append("Content-Type", "application/json");
   headers.append("enable_hr", "false");
@@ -100,101 +101,106 @@ console.log(input);
   
 
   return (
-    <Block>
-      <Block style={{display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center",marginTop:"10px",marginBottom:"10px",marginLeft:"20%"}}>
+        <Block $style={{ flex: 1, display: "flex", flexDirection: "column" }}>
+          <textarea value={input}  placeholder="Enter your prompt or choose from below" onChange={e => setInput(e.target.value)}  style={{display:"flex" , flexDirection:"column" , padding:"10px", margin:"10px" , borderRadius:"10px" , fontSize:15 , fontFamily:"sans-serif"}} />
+          <Button style={{margin:"20px"}} onClick={fetching}>Generate</Button>
+          <Block
+            $style={{
+              display: "flex",
+              alignItems: "center",
+              fontWeight: 500,
+              justifyContent: "space-between",
+              padding: "1.5rem",
+            }}
+          >
+            <Block>Images</Block>
+    
+            <Block onClick={() => setIsSidebarOpen(false)} $style={{ cursor: "pointer", display: "flex" }}>
+              <AngleDoubleLeft size={18} />
+            </Block>
+          </Block>
+          <Scrollable>
+            <Block padding="0 1.5rem">
+              <div style={{ display: "grid", gap: "8px", gridTemplateColumns: "1fr 1fr",}}>
+                {images.map((image, index) => {
+                  return (
+                  <div  className="tooltip" onClick={() => setInput(image.alt)}>
+                  <ImageItem key={index}   preview={image.src.small} />
+                  <span className="tooltiptext">{image.alt}</span>
+                  </div>
+                  )
+                })}
+              </div>
+            </Block>
+          </Scrollable>
+        </Block>
+      )
+    }
+    
+    const ImageItem = ({ preview, onClick }: { preview: any; onClick?: (option: any) => void }) => {
+      const [css] = useStyletron()
+      return (
+        <div
+          onClick={onClick}
+          className={css({
+            position: "relative",
+            background: "#f8f8fb",
+            cursor: "pointer",
+            borderRadius: "8px",
+            overflow: "hidden",
+            "::before:hover": {
+              opacity: 1,
+            },
+          })}
+        >
+          <div
+            className={css({
+              backgroundImage: `linear-gradient(to bottom,
+              rgba(0, 0, 0, 0) 0,
+              rgba(0, 0, 0, 0.006) 8.1%,
+              rgba(0, 0, 0, 0.022) 15.5%,
+              rgba(0, 0, 0, 0.047) 22.5%,
+              rgba(0, 0, 0, 0.079) 29%,
+              rgba(0, 0, 0, 0.117) 35.3%,
+              rgba(0, 0, 0, 0.158) 41.2%,
+              rgba(0, 0, 0, 0.203) 47.1%,
+              rgba(0, 0, 0, 0.247) 52.9%,
+              rgba(0, 0, 0, 0.292) 58.8%,
+              rgba(0, 0, 0, 0.333) 64.7%,
+              rgba(0, 0, 0, 0.371) 71%,
+              rgba(0, 0, 0, 0.403) 77.5%,
+              rgba(0, 0, 0, 0.428) 84.5%,
+              rgba(0, 0, 0, 0.444) 91.9%,
+              rgba(0, 0, 0, 0.45) 100%)`,
+              position: "absolute",
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              opacity: 0,
+              transition: "opacity 0.3s ease-in-out",
+              height: "100%",
+              width: "100%",
+              ":hover": {
+                opacity: 1,
+              },
+            })}
+          />
+         <div     className="tooltip">
       
-      <Block>
-      <Input onChange={(e)=> setInput(e.target.value)}/>
-      </Block>
-      <Button style={{height:"10%", maxHeight:"100px",marginTop:"10px"}} onClick={fetching}>Generate Button</Button>
-      </Block>
-    <Block $style={{ flex: 1, display: "flex", flexDirection: "column" }}>
-      <Block
-        $style={{
-          display: "flex",
-          alignItems: "center",
-          fontWeight: 500,
-          justifyContent: "space-between",
-          padding: "1.5rem",
-        }}
-      >
-        
-        <Block>Images</Block>
-
-        <Block onClick={() => setIsSidebarOpen(false)} $style={{ cursor: "pointer", display: "flex" }}>
-          <AngleDoubleLeft size={18} />
-        </Block>
-      </Block>
-      <Scrollable>
-        <Block padding="0 1.5rem">
-          <div style={{ display: "grid", gap: "8px", gridTemplateColumns: "1fr 1fr" }}>
-         
-          </div>
-        </Block>
-      </Scrollable>
-    </Block>
-    </Block>
-  )
-}
-
-const ImageItem = ({ preview, onClick }: { preview: any; onClick?: (option: any) => void }) => {
-  const [css] = useStyletron()
-  return (
-    <div
-      onClick={onClick}
-      className={css({
-        position: "relative",
-        background: "#f8f8fb",
-        cursor: "pointer",
-        borderRadius: "8px",
-        overflow: "hidden",
-        "::before:hover": {
-          opacity: 1,
-        },
-      })}
-    >
-      <div
-        className={css({
-          backgroundImage: `linear-gradient(to bottom,
-          rgba(0, 0, 0, 0) 0,
-          rgba(0, 0, 0, 0.006) 8.1%,
-          rgba(0, 0, 0, 0.022) 15.5%,
-          rgba(0, 0, 0, 0.047) 22.5%,
-          rgba(0, 0, 0, 0.079) 29%,
-          rgba(0, 0, 0, 0.117) 35.3%,
-          rgba(0, 0, 0, 0.158) 41.2%,
-          rgba(0, 0, 0, 0.203) 47.1%,
-          rgba(0, 0, 0, 0.247) 52.9%,
-          rgba(0, 0, 0, 0.292) 58.8%,
-          rgba(0, 0, 0, 0.333) 64.7%,
-          rgba(0, 0, 0, 0.371) 71%,
-          rgba(0, 0, 0, 0.403) 77.5%,
-          rgba(0, 0, 0, 0.428) 84.5%,
-          rgba(0, 0, 0, 0.444) 91.9%,
-          rgba(0, 0, 0, 0.45) 100%)`,
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          opacity: 0,
-          transition: "opacity 0.3s ease-in-out",
-          height: "100%",
-          width: "100%",
-          ":hover": {
-            opacity: 1,
-          },
-        })}
-      />
-      <div className="container">
       <img
         src={preview}
-        className="image"
+        className={css({
+          width: "100%",
+          height: "100%",
+          objectFit: "contain",
+          pointerEvents: "none",
+          verticalAlign: "middle",
+        })}
       />
-       <div className="overlay">My Name is John</div>
-    </div>
-    </div>
-  )
-}
+        </div>
+        </div>
+      )
+    }
 
 export default Images
